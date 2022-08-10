ae677d0e835a6f7e6fdb9c20ba91746d42fc6411
Artur Ciepielowski <artciepi@gmail.com>
2022-08-10 21:10:51 +0200

    First commit

---
+++ src/main/java/pl/GtoClass/whatWeWillEat/WhatWeWillEatApplication.java
@@ -0,0 +1,13 @@
+package pl.GtoClass.whatWeWillEat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatWeWillEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatWeWillEatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/GtoClass/whatWeWillEat/WhatWeWillEatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.GtoClass.whatWeWillEat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatWeWillEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
