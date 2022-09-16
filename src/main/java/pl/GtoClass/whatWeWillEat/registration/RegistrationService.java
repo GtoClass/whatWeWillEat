f6efd21efb7099090e6e0419b5de9fc418dac000
Artur Ciepielowski <artciepi@gmail.com>
2022-09-16 23:25:50 +0200

    Email verification

--- src/main/java/pl/GtoClass/whatWeWillEat/registration/RegistrationService.java
+++ src/main/java/pl/GtoClass/whatWeWillEat/registration/RegistrationService.java
@@ -1,10 +1,23 @@
 package pl.GtoClass.whatWeWillEat.registration;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.GtoClass.whatWeWillEat.appuser.AppUser;
+import pl.GtoClass.whatWeWillEat.appuser.AppUserService;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
-        return "it works";
+       boolean isValidEmail = emailValidator.test(request.getEmail());
+
+       if(!isValidEmail){
+           throw new IllegalStateException("email not valid");
+       }
+
+        return appUserService.signUpUser(new AppUser(request.getFirstName()));
     }
 }
