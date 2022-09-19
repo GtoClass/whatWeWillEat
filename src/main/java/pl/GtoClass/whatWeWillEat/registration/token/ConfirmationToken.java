5c21159bd76e2bb03791243e4b5fc58a321635c7
Artur Ciepielowski <artciepi@gmail.com>
2022-09-19 22:14:34 +0200

    Added token

---
+++ src/main/java/pl/GtoClass/whatWeWillEat/registration/token/ConfirmationToken.java
@@ -0,0 +1,49 @@
+package pl.GtoClass.whatWeWillEat.registration.token;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.GtoClass.whatWeWillEat.appuser.AppUser;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ConfirmationToken {
+
+    @SequenceGenerator(
+            name = "confirmation_token_sequence",
+            sequenceName = "confirmation_token_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "confirmation_token_sequence"
+    )
+
+
+    private Long id;
+    @Column(nullable = false)
+    private String token;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+    private LocalDateTime comfirmedAt;
+    @ManyToOne
+    @JoinColumn(nullable = false,
+    name = "app_user_id")
+
+    private AppUser appUser;
+
+    public ConfirmationToken(String token, LocalDateTime createdAt, LocalDateTime expiresAt, LocalDateTime comfirmedAt, AppUser appUser) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.comfirmedAt = comfirmedAt;
+        this.appUser = appUser;
+    }
+}
---
+++ src/main/java/pl/GtoClass/whatWeWillEat/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,7 @@
+package pl.GtoClass.whatWeWillEat.registration.token;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfirmationTokenService {
+}
